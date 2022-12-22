2e88030b1d7f61b9cd8e9ee146e517ce70b66db2
raj74434 <rajgupta74434@gmail.com>
2022-12-22 20:23:28 +0530

    project created

---
+++ demo/src/test/java/com/handy/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
