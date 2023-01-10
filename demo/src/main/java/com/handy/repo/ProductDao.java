0f2a266c7c99f73e7249daaa37d37a8466ffc6a8
raj74434 <rajgupta74434@gmail.com>
2023-01-11 00:43:43 +0530

    customer method added

--- demo/src/main/java/com/handy/repo/ProductDao.java
+++ demo/src/main/java/com/handy/repo/ProductDao.java
@@ -1,9 +1,15 @@
 package com.handy.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.handy.models.Product;
 
+@Repository
 public interface ProductDao extends JpaRepository<Product, Integer> {
 
+	public List<Product> findByProductName(String name);
+	
 }
--- demo/src/main/java/com/handy/services/ConsumerServiceImpll.java
+++ demo/src/main/java/com/handy/services/ConsumerServiceImpll.java
@@ -1,13 +1,30 @@
 package com.handy.services;
 
+import java.util.List;
+
 import com.handy.models.Consumer;
+import com.handy.models.Product;
+import com.handy.repo.ProductDao;
 
 public class ConsumerServiceImpll implements ConsumerService {
 
+	private ProductDao productDao;
+	
 	@Override
 	public Consumer registerConsumer() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public List<Product> searchProductByName(String name) throws Exception{
+		
+		List<Product> products= productDao.findByProductName(name);
+		
+		if(products.size()<1) {
+			throw new Exception();
+		}
+		
+		return products;
 	}
 
 }
